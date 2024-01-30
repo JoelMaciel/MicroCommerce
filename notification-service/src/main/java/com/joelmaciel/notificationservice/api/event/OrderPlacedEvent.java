8f5754b5227d5e8588479bf770112b18523e395b
Joel <joel.viana@audora.com.br>
2024-01-30 17:33:10 -0300

    feat: implementing topic with kafka and swagger for documentation

---
+++ notification-service/src/main/java/com/joelmaciel/notificationservice/api/event/OrderPlacedEvent.java
@@ -0,0 +1,12 @@
+package com.joelmaciel.notificationservice.api.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
